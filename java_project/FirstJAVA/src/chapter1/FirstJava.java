7e392f7bfa1097b4f3dfddf2382dd1c370746f18
Min <kas4947@gmail.com>
2020-04-08 20:40:40 +0900

    ..

--- java_project/FirstJAVA/src/chapter1/FirstJava.java
+++ java_project/FirstJAVA/src/chapter1/FirstJava.java
@@ -4,8 +4,16 @@ public class FirstJava {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World...!!! JAVA");
+		System.out.println("2+5=" + 2+5 );
+		System.out.println("2+5=" + (2+5) );
 		
+		int i = 15;
+		
+		System.out.println("15");
+		System.out.println("1" + 5);
+		System.out.println("1" + "5");
+		System.out.println(i);
+		System.out.println(1 + "5");
 	}
 
 }
