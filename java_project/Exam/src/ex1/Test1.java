38e7bd3f3fc5e28b34a98e90eba980aee6d18fdf
Min <kas4947@gmail.com>
2020-04-09 10:12:55 +0900

    Create Test1.java

---
+++ java_project/Exam/src/ex1/Test1.java
@@ -0,0 +1,17 @@
+package ex1;
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		
+		/*
+			문제1
+			다음 두 문장을 출력하는 프로그램을 작성ㅇ해 보자.
+		 *
+		 */
+		
+		System.out.println("2+5= " + 2+5);
+		System.out.println("2+5= " + (2+5));
+	}
+
+}
