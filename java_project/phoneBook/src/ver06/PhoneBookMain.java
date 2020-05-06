0d03f885564fb659472379ebc942374186e91ccb
Min <kas4947@gmail.com>
2020-05-06 19:55:59 +0900

    mainGuide

--- java_project/phoneBook/src/ver06/PhoneBookMain.java
+++ java_project/phoneBook/src/ver06/PhoneBookMain.java
@@ -17,25 +17,23 @@ public class PhoneBookMain {
 
                 // 정상 범위는 1~6
                 // MenuNum.CREATE~MenuNum.EXIT
-                if(!(select >= MenuNum.CREATE && select <= MenuNum.EXIT)){
+                if (!(select >= MenuNum.CREATE && select <= MenuNum.EXIT)) {
                     BadNumberException e = new BadNumberException("메뉴 범위를 벗어나는 경우입니다. \n 다시 확인후 입력해주세요.");
 
-                            //강제적인 예외 발생;
+                    //강제적인 예외 발생;
                     throw e;
                 }
             } catch (InputMismatchException e) {
                 System.out.println("잘못된 메뉴 입력입니다. \n확인하시고 다시 입력해주세요.");
                 //manager.sc.nextLine();
                 continue;
-            } catch (BadNumberException e){
+            } catch (BadNumberException e) {
                 System.out.println("메뉴범위를 벗어난 숫자 입력입니다. \n 다시 확인 후 입력해주세요.");
                 continue;
-            }
-            catch (Exception e){
+            } catch (Exception e) {
                 System.out.println("잘못된 메뉴입력입니다.");
                 continue;
-            }
-            finally {
+            } finally {
                 manager.sc.nextLine();
             }
 
