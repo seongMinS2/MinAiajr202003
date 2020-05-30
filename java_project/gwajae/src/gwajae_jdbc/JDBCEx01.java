032dd3cda6a9757df497c8b74e23348e107e6899
Min <kas4947@gmail.com>
2020-05-30 16:26:19 +0900

    5.30

--- java_project/gwajae/src/gwajae_jdbc/JDBCEx01.java
+++ java_project/gwajae/src/gwajae_jdbc/JDBCEx01.java
@@ -27,7 +27,7 @@ public class JDBCEx01 {
 
 			// 3. statment
 
-			// 3.1 테이블 복사부터 하고 시작함.
+			// 테이블 복사부터 하고 시작함.
 
 //			String sql = "create table emp10 as select * from emp";
 //			pstmt = conn.prepareStatement(sql);
@@ -38,7 +38,7 @@ public class JDBCEx01 {
 //			if(rs=false) 
 //				System.out.println("테이블이 성공적으로 복사되었습니다.");
 
-			// 1. EMP 테이블에 새로운 사원 정보를 입력하는 프로그램을 작성해보자.
+			// 3.1 EMP 테이블에 새로운 사원 정보를 입력하는 프로그램을 작성해보자.
 
 //			System.out.println("사원번호를 입력하세요.");
 //			int empno = sc.nextInt();
@@ -85,28 +85,90 @@ public class JDBCEx01 {
 //			
 //			System.out.println(rs + "개 행이 입력 되었습니다.");
 
-			// 2. EMP 테이블의 모든 데이터를 출력하는 프로그램을 작성해보자.
-			String sql = "select * from emp10";
+			// 3.2 EMP 테이블의 모든 데이터를 출력하는 프로그램을 작성해보자.
+
+//			String sql = "select * from emp10";
+//			pstmt = conn.prepareStatement(sql);
+//
+//			ResultSet rs = pstmt.executeQuery(sql);
+//
+//			while (rs.next()) {
+//				int empno = rs.getInt(1);
+//				String ename = rs.getString(2);
+//				String job = rs.getString(3);
+//				int mgr = rs.getInt(4);
+//				Date hireDate = rs.getDate(5);
+//				int sal = rs.getInt(6);
+//				int comm = rs.getInt(7);
+//				int deptno = rs.getInt(8);
+//
+//				System.out.println(empno + "\t" + ename + "\t" + job + "\t" + mgr + "\t" + hireDate + "\t" + sal + "\t"
+//						+ comm + "\t" + deptno);
+//			}
+			
+			
+			//3.3 EMP 테이블에 서 “SCOTT” 사원의 급여(sal) 정보를 1000으로 바꾸는 프로그램을 작성해보자. 
+
+//			String sql = "update emp10 set sal = 1000 where ename = 'SCOTT' " ;
+//			pstmt = conn.prepareStatement(sql);
+//			
+//			int count = pstmt.executeUpdate();
+//			
+//			System.out.println( count + "개 행이 입력되었습니다.");
+
+			
+			//3.4EMP 테이블에 서 “SCOTT” 이름으로 검색한 결과를 출력하는 프로그램을 작성해보자. 
+			
+//			String sql = "select * from emp10 where ename = 'SCOTT'";
+//			
+//			pstmt = conn.prepareStatement(sql);
+//			
+//			ResultSet rs = pstmt.executeQuery(sql);
+//			
+//			while (rs.next()) {
+//			int empno = rs.getInt(1);
+//			String ename = rs.getString(2);
+//			String job = rs.getString(3);
+//			int mgr = rs.getInt(4);
+//			Date hireDate = rs.getDate(5);
+//			int sal = rs.getInt(6);
+//			int comm = rs.getInt(7);
+//			int deptno = rs.getInt(8);
+//
+//			System.out.println(empno + "\t" + ename + "\t" + job + "\t" + mgr + "\t" + hireDate + "\t" + sal + "\t"
+//					+ comm + "\t" + deptno);
+//		}
+			
+			//3.5 모든 사원정보를 출력하되 부서정보를 함께 출력하는 프로그램을 작성해보자. 
+			
+			String sql = "select * from emp natural join dept";
+			
 			pstmt = conn.prepareStatement(sql);
-
+			
 			ResultSet rs = pstmt.executeQuery(sql);
-
+			
 			while (rs.next()) {
-				int empno = rs.getInt(1);
-				String ename = rs.getString(2);
-				String job = rs.getString(3);
-				int mgr = rs.getInt(4);
-				Date hireDate = rs.getDate(5);
-				int sal = rs.getInt(6);
-				int comm = rs.getInt(7);
-				int deptno = rs.getInt(8);
-				
-				System.out.println(empno + "\t" + ename + "\t" + job + "\t" + mgr + "\t" + hireDate + "\t" + sal + "\t" + comm + "\t" + deptno);
-			}
+			int deptno = rs.getInt(1);
+			int empno = rs.getInt(2);
+			String ename = rs.getString(3);
+			String job = rs.getString(4);
+			int mgr = rs.getInt(5);
+			Date hireDate = rs.getDate(6);
+			int sal = rs.getInt(7);
+			int comm = rs.getInt(8);
+			String dname = rs.getString(9);
+			String log = rs.getString(10);
 
+			System.out.println(deptno + "\t" + empno + "\t" + ename + "\t" + job + "\t" + mgr + "\t" + hireDate + "\t"
+					+ sal + "\t" + comm + "\t" + dname + "\t" + log);
+		}
+			
+			
+			//4. 사용된 자원들 종료
+			
+			rs.close();
 			conn.close();
 			pstmt.close();
-
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
