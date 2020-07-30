d9face7f71334912183d248b584c0cceb5af5e54
Min <kas4947@gmail.com>
2020-07-30 20:14:59 +0900

    7/30
    7/30

---
+++ spring_project/Spring-MVC/src/main/java/com/aia/mvc/login/LoginFormController.java
@@ -0,0 +1,59 @@
+package com.aia.mvc.login;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.aia.mvc.login.model.LoginRequest;
+
+@Controller
+@RequestMapping("/login/login") // http://localhost:8080/mvc/login/login
+public class LoginFormController {
+	
+	// get 방식의 요청에 대한 처리 -> 로그인 폼 페이지
+	@RequestMapping(method = RequestMethod.GET)
+	public String getLoginForm() {
+		return "login/loginForm";
+		// /WEB-INF/views/login/login.jsp
+	}
+	
+	// Post 방식의 요청 처리 메서드
+	@RequestMapping(method = RequestMethod.POST)
+	
+	public String login(HttpServletRequest request,
+			@RequestParam("uid") String userId,
+			@RequestParam("upw") String userPw,
+			@ModelAttribute("login") LoginRequest loginRequest,
+			Model model) {
+			
+		String uid = request.getParameter("uid");
+		String upw = request.getParameter("upw");
+		
+		System.out.println(uid);
+		System.out.println(upw);
+		
+		System.out.println("-----------------");
+		
+		System.out.println(userId);
+		System.out.println(userPw);
+		
+		System.out.println("------------------");
+		
+		System.out.println(loginRequest.getUid());
+		System.out.println(loginRequest.getUpw());
+		
+		request.setAttribute("uid", uid);
+		request.setAttribute("upw", upw);
+		
+		model.addAttribute("uid", uid);
+		model.addAttribute("upw", upw);
+		
+		return "login/login";
+	}
+	
+}
---
+++ spring_project/SpringDI/src/di/main/MainForSpringJavaConfig.java
@@ -0,0 +1,26 @@
+package di.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import di.config.JavaConfig;
+import di.dao.MemberDao;
+import di.service.ChangePasswordService;
+import di.service.MemberRegisterService;
+
+public class MainForSpringJavaConfig {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(JavaConfig.class);
+		
+		MemberRegisterService regSvc = ctx.getBean("regSvc", MemberRegisterService.class);
+		System.out.println(regSvc);
+		MemberDao dao = ctx.getBean("dao",MemberDao.class);
+		System.out.println(dao);
+		ChangePasswordService pwSvc = ctx.getBean("changePwSvc", ChangePasswordService.class);
+		System.out.println(pwSvc);
+		
+	}
+
+}
